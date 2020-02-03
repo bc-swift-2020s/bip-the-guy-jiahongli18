eb41379e4213ee58f4466566d8a9b3fd5a8b9271
Jiahong Li <libya@bc.edu>
2020-02-03 12:53:51 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Jia H Li on 2/3/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
